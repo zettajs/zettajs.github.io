26929b400f0b6f33c616f4e09e247388c3764cd4
Alan Languirand <alan@13protons.com>
2014-09-23 16:41:07 -0400

    Recipe Template

--- build/dev/js/zetta.js
+++ build/dev/js/zetta.js
@@ -7,8 +7,11 @@
 hljs.initHighlightingOnLoad();
 
 $(function(){
-   var mqbreak = 100;
-  //TODO: inventory images in features and make sure dimensions are set for skrollr
+  
+  //github style header linking in articles
+  $('article').find('h1, h2, h3, h4, h5, h6').click(function(){
+    window.location.hash = $(this).attr('id');
+  });
   
   //auto select npm install text on homepage
   $('.install input').on('click focus', function(){$(this).select();});
@@ -32,72 +35,8 @@ $(function(){
     }
   });
   
-  /*  
-      Hijack Prism's line highlighter metrics and cast as a background image for z-index-free color layering
-      The background image is specified in css as a linear gradient between two identical colors @0,0, height: 0; 
-  */
-  
-  window.setTimeout(mqtest, 50);
-  updateArrows();
-  $(window).resize(function(){
-      mqtest();
-      updateArrows();
-  }); //window resize
-  
-  function mqtest(){
-    var mqlevel = $('#mqtest').css('z-index');
-    if(mqlevel != mqbreak){
-      mqbreak = mqlevel;
-      $('#mqtest').val(mqlevel);
-      $('#mqtest').trigger('change');
-    } 
-  }
   
   
-  function updateArrows(){
-    
-    
-    //css media query breakpoints change the border color. Easier than doing math on the window, and keeps mq metrics where they belong!
-    /*
-    
-    if($('.hilight').first().find('.arrow').css('border-left-color') != 'rgba(0, 0, 0, 0)'){
-      $('.line-highlight').remove();
-      
-      Prism.highlightAll(false, function(){
-          $('.line-highlight').each(function(){
-            //sniff out the metrics we need
-            var hl = {
-              pre: $(this).parent().parent(), // we can do this b/c prism only works with a <code> inside a <pre>
-              size: $(this).height(),
-              half_size: $(this).height()/2,
-              top: parseInt($(this).css('top')) -1
-            };
-            //add the <pre>'s padding-top to the offset for the image
-            hl.padding_top = parseInt(hl.pre.css('padding-top'));
-            hl.padding_left = parseInt(hl.pre.css('padding-left'));
-            hl.offset = hl.top + hl.padding_top;
-
-            //move default hidden bg image into position: 
-            hl.pre.css('background-size', '100% ' + hl.size + 'px');
-            hl.pre.css('background-position', '0 ' + hl.offset + 'px');
-
-            //resize the css arrow
-            hl.pre.find('.arrow')
-              .css('border-top-width', hl.half_size + 'px')
-              .css('border-bottom-width', hl.half_size + 'px')
-              .css('margin-top', hl.top + 'px')
-              .css('margin-left', (hl.pre.width() + hl.padding_left) + 'px');
-
-            //un-hide the css arrow
-            hl.pre.addClass('hilight');
-          }); //.line-highlight .each
-      }); //Prism.highlightAll
-      
-    }else {
-      $('pre.arrow').removeAttr('style');
-    }
-*/
-  }
   
   
 });
